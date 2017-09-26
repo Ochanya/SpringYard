046b999683cbbef31a125a0951872175a62ca3d6
Your Name <you@example.com>
2017-09-26 05:16:45 -0400

    initial commit

---
+++ src/main/java/com/example/customer/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.example.customer.repository;
+
+import com.example.customer.model.Customer;
+
+
+import java.util.List;
+
+
+public interface CustomerRepository {
+  void add(Customer customer);
+  void update(Customer customer);
+  Customer getById(int id);
+  List<Customer> get();
+  void delete(int id);
+}
